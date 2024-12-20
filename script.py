5554f65d92a30d452c677e5747c0ff57b24737b9
AmarildoCaka <139240882+AmarildoCaka@users.noreply.github.com>
2024-12-20 20:42:00 +0100

    modified the python file!

--- script.py
+++ script.py
@@ -1,9 +1,7 @@
 def add_two_numbers(firstNum, secondNum):
-
-    message = "adding"
-
-    print(f"This is the ${message} of two numbers!")
     
     print(firstNum + secondNum)
+    
+    return firstNum + secondNum
     
 add_two_numbers(100, 100)
