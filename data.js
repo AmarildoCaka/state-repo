b1e910296d627a3dc610fbf01539493e96a80f57
AmarildoCaka <139240882+AmarildoCaka@users.noreply.github.com>
2024-12-20 20:37:40 +0100

    I created this repo and added these documents!

---
+++ data.js
@@ -0,0 +1,1 @@
+const added = 100;
---
+++ script.py
@@ -0,0 +1,9 @@
+def add_two_numbers(firstNum, secondNum):
+
+    message = "adding"
+
+    print(f"This is the ${message} of two numbers!")
+    
+    print(firstNum + secondNum)
+    
+add_two_numbers(100, 100)
